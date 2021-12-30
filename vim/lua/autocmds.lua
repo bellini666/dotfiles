7cdde96add1418cd0362690e7da279d8930f47db
Thiago Bellini Ribeiro <thiago@bellini.dev>
2021-12-30 10:26:22 -0300

    Don't clear buffer autocmds

--- vim/lua/autocmds.lua
+++ vim/lua/autocmds.lua
@@ -22,7 +22,6 @@ M.setup_lsp = function(client, bufnr)
     if client.resolved_capabilities.document_formatting then
         vim.cmd([[
         augroup __lsp_document_format
-          autocmd!
           autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1500)
         augroup END
         ]])
@@ -31,7 +30,6 @@ M.setup_lsp = function(client, bufnr)
     if client.resolved_capabilities.document_highlight then
         vim.cmd([[
         augroup __lsp_document_highlight
-          autocmd!
           autocmd CursorHold,CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
           autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
         augroup END
@@ -41,7 +39,6 @@ M.setup_lsp = function(client, bufnr)
     if client.resolved_capabilities.code_lens then
         vim.cmd([[
         augroup __lsp_code_lens_refresh
-          autocmd!
           autocmd BufEnter,CursorHold,InsertLeave <buffer> lua vim.lsp.codelens.refresh()
         augroup END
         ]])
