243b9c9026be78862ff31696a2165623ea980493
Thiago Bellini Ribeiro <thiago@bellini.dev>
2022-01-08 19:20:06 -0300

    Fix autocmds registering many times

--- vim/lua/autocmds.lua
+++ vim/lua/autocmds.lua
@@ -1,4 +1,5 @@
 local M = {}
+local registered_cache = {}
 
 vim.cmd([[
 augroup __autocmds
@@ -20,29 +21,39 @@ augroup END
 ]])
 
 M.setup_lsp = function(client, bufnr)
-    if client.resolved_capabilities.document_formatting then
+    -- We can't clear autocmds because null-ls doesn't register itself anymore...
+    local cache = registered_cache[bufnr]
+    if cache == nil then
+        cache = {}
+        registered_cache[bufnr] = cache
+    end
+
+    if not cache["formatting"] and client.resolved_capabilities.document_formatting then
         vim.cmd([[
         augroup __lsp_document_format
           autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1500)
         augroup END
         ]])
+        cache["formatting"] = true
     end
 
-    if client.resolved_capabilities.document_highlight then
+    if not cache["highlight"] and client.resolved_capabilities.document_highlight then
         vim.cmd([[
         augroup __lsp_document_highlight
           autocmd CursorHold,CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
           autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
         augroup END
         ]])
+        cache["highlight"] = true
     end
 
-    if client.resolved_capabilities.code_lens then
+    if not cache["code_lens"] and client.resolved_capabilities.code_lens then
         vim.cmd([[
         augroup __lsp_code_lens_refresh
           autocmd BufEnter,CursorHold,InsertLeave <buffer> lua vim.lsp.codelens.refresh()
         augroup END
         ]])
+        cache["code_lens"] = true
     end
 end
 
