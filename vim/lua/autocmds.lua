72345a8732e0869e16b6efb34aa367179c87a0c6
Thiago Bellini Ribeiro <thiago@bellini.dev>
2022-01-08 01:59:09 -0300

    Restore old autocmds behaviour

--- vim/lua/autocmds.lua
+++ vim/lua/autocmds.lua
@@ -1,7 +1,5 @@
 local M = {}
 
-local caps_available = {}
-
 vim.cmd([[
 augroup __autocmds
   autocmd!
@@ -21,55 +19,30 @@ augroup __autocmds
 augroup END
 ]])
 
-M.cap_exec = function(bufnr, cap, func, ...)
-    if caps_available[bufnr] == nil then
-        caps_available[bufnr] = {}
-    end
-    local active_tbl = caps_available[bufnr][cap]
-    if not active_tbl or vim.tbl_isempty(active_tbl) then
-        return
-    end
-    return func(...)
-end
-
 M.setup_lsp = function(client, bufnr)
-    if caps_available[bufnr] == nil then
-        caps_available[bufnr] = {}
-    end
-    local bcaps = caps_available[bufnr]
-
-    for k, v in pairs(client.resolved_capabilities) do
-        if bcaps[k] == nil then
-            bcaps[k] = {}
-        end
-        bcaps[k][client.name] = v or nil
+    if client.resolved_capabilities.document_formatting then
+        vim.cmd([[
+        augroup __lsp_document_format
+          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1500)
+        augroup END
+        ]])
     end
 
-    vim.cmd("augroup __lsp_cmds")
-    vim.cmd("autocmd!")
-
-    local _cmd = function(event, cap, func, opts)
-        local args = string.format("%s, '%s', %s", bufnr, cap, func)
-        if opts ~= nil then
-            args = args .. ", " .. opts
-        end
-        return vim.cmd(
-            string.format('autocmd %s <buffer> lua require("autocmds").cap_exec(%s)', event, args)
-        )
+    if client.resolved_capabilities.document_highlight then
+        vim.cmd([[
+        augroup __lsp_document_highlight
+          autocmd CursorHold,CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
+          autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+        augroup END
+        ]])
     end
-    _cmd("BufWritePre", "document_formatting", "vim.lsp.buf.formatting_sync", "{ nil, 1500 }")
-    _cmd("CursorHold,CursorHoldI", "document_highlight", "vim.lsp.buf.document_highlight")
-    _cmd("CursorMoved", "document_highlight", "vim.lsp.buf.clear_references")
-    _cmd("BufEnter,CursorHold,CursorHoldI,InsertLeave", "code_lens", "vim.lsp.codelens.refresh")
 
-    vim.cmd("augroup END")
-
-    local original_stop = client.stop
-    client.stop = function(...)
-        for _, v in pairs(bcaps) do
-            v[client.name] = nil
-        end
-        original_stop(...)
+    if client.resolved_capabilities.code_lens then
+        vim.cmd([[
+        augroup __lsp_code_lens_refresh
+          autocmd BufEnter,CursorHold,InsertLeave <buffer> lua vim.lsp.codelens.refresh()
+        augroup END
+        ]])
     end
 end
 
