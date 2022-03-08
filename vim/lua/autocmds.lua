2d08bffdd1bb250322161f9e71eb92f70f05dc55
Thiago Bellini Ribeiro <thiago@bellini.dev>
2022-03-07 21:17:51 -0300

    Migrate autocmds to the new lua api

--- vim/lua/autocmds.lua
+++ vim/lua/autocmds.lua
@@ -1,47 +1,43 @@
 local M = {}
 
-vim.cmd([[
-augroup __autocmds
-  autocmd!
-
-  " Packer
-  autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-
-  " Filetypes
-  autocmd FileType * lua require("options").setup_ft()
-
-augroup END
-]])
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "plugins.lua",
+  callback = function()
+    vim.cmd("source <afile>")
+    vim.cmd("PackerCompile")
+  end,
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = require("options").setup_ft,
+})
 
 M.setup_lsp = function(client, bufnr)
   if client.resolved_capabilities.document_formatting then
-    vim.cmd([[
-      augroup __lsp_document_format
-        autocmd! * <buffer>
-        autocmd BufWritePre <buffer> lua require("utils").lsp_format()
-      augroup END
-    ]])
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = require("utils").lsp_format,
+    })
   end
 
   if client.resolved_capabilities.code_lens then
-    vim.cmd([[
-      augroup __lsp_code_lens_refresh
-        autocmd! * <buffer>
-        autocmd BufEnter,CursorHold,InsertLeave <buffer> lua vim.lsp.codelens.refresh()
-      augroup END
-    ]])
+    vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+      buffer = bufnr,
+      callback = vim.lsp.codelens.refresh,
+    })
   end
 
   -- Not using this right now as it is annoying
-  -- if client.resolved_capabilities.document_highlight then
-  --   vim.cmd([[
-  --     augroup __lsp_document_highlight
-  --       autocmd! * <buffer>
-  --       autocmd CursorHold,CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
-  --       autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-  --     augroup END
-  --   ]])
-  -- end
+  if client.resolved_capabilities.document_highlight and false then
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      buffer = bufnr,
+      callback = vim.lsp.buf.document_highlight,
+    })
+    vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+      buffer = bufnr,
+      callback = vim.lsp.buf.clear_references,
+    })
+  end
 end
 
 return M
