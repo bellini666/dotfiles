d1165a33886ac72e6655936b82fdd3a17da6c783
Thiago Bellini Ribeiro <thiago@bellini.dev>
2022-03-31 21:15:35 -0300

    Use augroup to avoid registering an autocmd more than once

--- vim/lua/autocmds.lua
+++ vim/lua/autocmds.lua
@@ -14,28 +14,44 @@ vim.api.nvim_create_autocmd("FileType", {
 
 M.setup_lsp = function(client, bufnr)
   if client.resolved_capabilities.document_formatting then
+    local group_id = vim.api.nvim_create_augroup(
+      ("_lsp_formatting_%d"):format(bufnr),
+      { clear = true }
+    )
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
       callback = require("utils").lsp_format,
+      group = group_id,
     })
   end
 
   if client.resolved_capabilities.code_lens then
+    local group_id = vim.api.nvim_create_augroup(
+      ("_lsp_codelens_%d"):format(bufnr),
+      { clear = true }
+    )
     vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
       buffer = bufnr,
       callback = vim.lsp.codelens.refresh,
+      group = group_id,
     })
   end
 
   -- Not using this right now as it is annoying
   if client.resolved_capabilities.document_highlight and false then
+    local group_id = vim.api.nvim_create_augroup(
+      ("_lsp_highlight_%d"):format(bufnr),
+      { clear = true }
+    )
     vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
       buffer = bufnr,
       callback = vim.lsp.buf.document_highlight,
+      group = group_id,
     })
     vim.api.nvim_create_autocmd({ "CursorMoved" }, {
       buffer = bufnr,
       callback = vim.lsp.buf.clear_references,
+      group = group_id,
     })
   end
 end
