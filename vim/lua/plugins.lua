e2d2d78b1c54c76042696895bd668b5dd9cd006e
Thiago Bellini Ribeiro <thiago@bellini.dev>
2022-08-21 11:55:04 -0300

    feat: Improve vim multiple cursors config

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -311,6 +311,9 @@ return packer.startup(function(use, use_rocks)
       vim.g.VM_Extend_hl = "ThemerSearchResult"
       vim.g.VM_Cursor_hl = "ThemerSearchResult"
       vim.g.VM_Insert_hl = "Cursor"
+      vim.g.VM_silent_exit = 1
+      vim.g.VM_quit_after_leaving_insert_mode = 1
+      vim.g.VM_show_warnings = 0
     end,
     branch = "master",
     requires = "themercorp/themer.lua",
