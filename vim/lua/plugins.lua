9fa75ed8bd01e6ad5924ef217c4647dc2b20bde4
Thiago Bellini Ribeiro <thiago@bellini.dev>
2023-01-27 18:28:37 -0300

    fix: add missing build command for luasnip

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -89,6 +89,7 @@ return {
           "saadparwaiz1/cmp_luasnip",
           "rafamadriz/friendly-snippets",
         },
+        build = "make install_jsregexp",
         config = function()
           require("luasnip.loaders.from_vscode").lazy_load()
         end,
