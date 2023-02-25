0a4f05e2759a82620b5bd81907b07c82cdc9ce5a
Thiago Bellini Ribeiro <thiago@bellini.dev>
2023-02-25 13:01:49 -0300

    refactor: reduce dap logging level

--- vim/lua/config/dap.lua
+++ vim/lua/config/dap.lua
@@ -2,7 +2,6 @@ local dap = require("dap")
 local utils = require("utils")
 local dap_python = require("dap-python")
 
-require("dap").set_log_level("TRACE")
 dap_python.setup("~/.debugpy/bin/python3", {})
 dap_python.test_runner = "pytest"
 dap_python.resolve_python = function()
