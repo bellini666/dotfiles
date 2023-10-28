3bf905d96603bd6ec6b1facdcc4c5b2fe0351ba7
Thiago Bellini Ribeiro <thiago@bellini.dev>
2023-10-28 11:58:12 -0300

    refactor: support running pyright in a poetry project that doesn't use .venv for the venv dir

--- vim/lua/config/lsp.lua
+++ vim/lua/config/lsp.lua
@@ -152,7 +152,17 @@ nvim_lsp.pyright.setup({
     if vim.env.VIRTUAL_ENV then
       p = require("null-ls.utils").path.join(vim.env.VIRTUAL_ENV, "bin", "python3")
     else
-      p = utils.find_cmd("python3", ".venv/bin")
+      local env_info = nil
+      if utils.find_file("poetry.lock") then
+        env_info =
+          vim.fn.system({ "poetry", "env", "info", "--path", "-C", vim.api.nvim_buf_get_name(0) })
+      end
+
+      if env_info ~= nil and string.find(env_info, "could not find") == nil then
+        p = require("null-ls.utils").path.join(p, "bin", "python3")
+      else
+        p = utils.find_cmd("python3", ".venv/bin")
+      end
     end
     config.settings.python.pythonPath = p
   end,
