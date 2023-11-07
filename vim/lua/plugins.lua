59c602fb1c94ddbe6638827af40c7df27297acf5
Thiago Bellini Ribeiro <thiago@bellini.dev>
2023-11-07 09:42:21 -0300

    feat: configure user commands for peek

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -456,6 +456,11 @@ return {
     "toppair/peek.nvim",
     build = "deno task --quiet build:fast",
     ft = "markdown",
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
   },
 
   -- Text editing
