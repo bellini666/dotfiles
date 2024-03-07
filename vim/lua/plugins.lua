8ee0851c89c7567426dfe08efe0ce4c67e3a179f
Thiago Bellini Ribeiro <thiago@bellini.dev>
2024-03-07 13:05:55 -0300

    feat: setup chatgpt plugin

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -142,15 +142,28 @@ return {
         auto_trigger = true,
         accept = false,
         keymap = {
-          accept = "<C-CR>",
+          accept = "<C-.>",
           next = "<C-]>",
           prev = "<C-[>",
-          dismiss = "<C-.>",
+          dismiss = "<C-,>",
         },
       },
       filetypes = {
         ["*"] = true,
       },
+    },
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim",
     },
   },
 
