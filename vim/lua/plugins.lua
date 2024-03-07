c38eafe43ba0435e9b85427f3d1b54eb65a65736
Thiago Bellini Ribeiro <thiago@bellini.dev>
2024-03-07 20:51:42 -0300

    feat: use 1password to retrieve openai key

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -145,10 +145,12 @@ return {
   },
   {
     "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
     config = function()
-      require("chatgpt").setup()
+      require("chatgpt").setup({
+        api_key_cmd = 'op read "op://private/OpenAI API Key/api key" --no-newline',
+      })
     end,
+    cmd = { "ChatGPT", "ChatGPTEditWithInstruction", "ChatGPTRun" },
     dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
