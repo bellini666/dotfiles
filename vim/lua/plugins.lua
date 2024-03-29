6852f6977dd510aebf3c2acde7846e6969bd8f51
Thiago Bellini Ribeiro <thiago@bellini.dev>
2024-03-29 12:51:02 -0300

    feat: remove chatgpt plugin, not actually using it

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -143,21 +143,6 @@ return {
       },
     },
   },
-  {
-    "jackMort/ChatGPT.nvim",
-    config = function()
-      require("chatgpt").setup({
-        api_key_cmd = 'op read "op://private/OpenAI API Key/api key" --no-newline',
-      })
-    end,
-    cmd = { "ChatGPT", "ChatGPTEditWithInstruction", "ChatGPTRun" },
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
 
   -- Testing
   {
