e0eb4b56f60e41989ae395e8409e1ddf882eab36
Thiago Bellini Ribeiro <thiago@bellini.dev>
2024-05-18 11:13:06 -0300

    refactor: use vim.diagnostic.config

--- vim/lua/config/lsp.lua
+++ vim/lua/config/lsp.lua
@@ -21,11 +21,16 @@ local lsp_capabilities = function()
   return capabilities
 end
 
-local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "",
+    },
+  },
+})
 
 local on_attach = function(client, bufnr)
   if client.server_capabilities.documentSymbolProvider then
