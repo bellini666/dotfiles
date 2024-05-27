5af5e4b57b98df39044dab3d55ac8f60ac46d52a
Thiago Bellini Ribeiro <thiago@bellini.dev>
2024-05-27 12:30:12 +0100

    feat: add focus.nvim plugin

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -313,6 +313,19 @@ return {
     },
   },
   {
+    "nvim-focus/focus.nvim",
+    opts = {
+      autoresize = {
+        width = 105,
+      },
+      ui = {
+        number = true,
+        cursorline = false,
+        signcolumn = false,
+      },
+    },
+  },
+  {
     "SmiteshP/nvim-navic",
     lazy = true,
     dependencies = {
