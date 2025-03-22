07c006409656614cba7a705a86e17cdecf0bd339
Thiago Bellini Ribeiro <thiago@bellini.dev>
2025-03-22 12:46:30 +0100

    fix: remove forgotten dressing on another plugin's dependency list

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -386,7 +386,6 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
     },
     opts = {
       strategies = {
