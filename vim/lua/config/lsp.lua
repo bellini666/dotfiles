7b279ac7dbc42824039bbc998d772eafb1c5a0c2
Thiago Bellini Ribeiro <thiago@bellini.dev>
2025-09-05 17:36:26 +0200

    feat: proper ty configuration as lsp

--- vim/lua/config/lsp.lua
+++ vim/lua/config/lsp.lua
@@ -163,10 +163,20 @@ local python_lsp = os.getenv("PYTHON_LSP") or "pyright"
 if python_lsp == "ty" then
   -- https://github.com/astral-sh/ty
   enable("ty", {
+    before_init = function(initialize_params, config)
+      local python_path = utils.find_python()
+      utils.ensure_tables(config, "settings", "ty")
+      config.settings.ty.interpreter = python_path
+      utils.ensure_tables(initialize_params, "initializationOptions", "settings", "ty")
+      initialize_params.initializationOptions.settings.ty.interpreter = python_path
+    end,
     settings = {
       ty = {
         importStrategy = "fromEnvironment",
         diagnosticMode = "workspace",
+        experimental = {
+          rename = true,
+        },
       },
     },
   })
