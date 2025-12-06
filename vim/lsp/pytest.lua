7d0810d6f7e783962035ed4251fa119d7e77feec
Thiago Bellini Ribeiro <thiago@bellini.dev>
2025-12-06 22:16:24 +0100

    refactor: use pytest-language-server from PATH

--- vim/lsp/pytest.lua
+++ vim/lsp/pytest.lua
@@ -1,7 +1,6 @@
 ---@type vim.lsp.Config
 return {
-  -- cmd = { "pytest-language-server" },
-  cmd = { "/Users/bellini/dev/pytest-language-server/target/release/pytest-language-server" },
+  cmd = { "pytest-language-server" },
   filetypes = { "python" },
   root_markers = { ".pytest.ini", "tox.ini", "setup.cfg", "setup.py", "pyproject.toml", ".git" },
   settings = {},
