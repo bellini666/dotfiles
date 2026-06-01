1370c9053e6321852a1bb3e534be5799afc12228
Thiago Bellini Ribeiro <thiago@bellini.dev>
2026-06-01 18:55:25 +0200

    chore: remove workaround from dropbar

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -270,17 +270,6 @@ return {
       menu = {
         preview = false,
       },
-      bar = {
-        -- nvim master removed BufModifiedSet (#35610); override dropbar's
-        -- default buf list, which still references it.
-        update_events = {
-          buf = {
-            "FileChangedShellPost",
-            "TextChanged",
-            "ModeChanged",
-          },
-        },
-      },
     },
   },
   {
